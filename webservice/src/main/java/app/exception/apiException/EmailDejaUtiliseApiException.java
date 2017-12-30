412a34eed90dac54f64c037b49aade12678c9ca1
Louis-Maxime <louis-maxime.credeville@etu.univ-orleans.fr>
2017-12-30 18:32:53 +0100

    Changement code exception

--- webservice/src/main/java/app/exception/apiException/EmailDejaUtiliseApiException.java
+++ webservice/src/main/java/app/exception/apiException/EmailDejaUtiliseApiException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public class EmailDejaUtiliseApiException extends ApiException {
 
-    public final static int CODE = 9;
+    public final static int CODE = 10;
     public final static HttpStatus HTTP_STATUS = HttpStatus.BAD_REQUEST;
 
 
