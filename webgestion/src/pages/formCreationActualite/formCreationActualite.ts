df681e43002b70f7a202d22b71ae414d089954d7
Louis-Maxime <louis-maxime.credeville@etu.univ-orleans.fr>
2018-01-08 21:03:24 +0100

    add actualite webservice

--- webgestion/src/pages/formCreationActualite/formCreationActualite.ts
+++ webgestion/src/pages/formCreationActualite/formCreationActualite.ts
@@ -1,24 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Actualite, Sport} from "../../common/model";
+import {WebserviceProvider} from "../../common/webservice";
 
 @Component({
-	selector: 'page-formcreationactualite',
-	templateUrl: 'formCreationActualite.html'
+  selector: 'page-formcreationactualite',
+  templateUrl: 'formCreationActualite.html'
 })
-export class formCreationActualitePage {
+export class formCreationActualitePage implements OnInit {
+  ngOnInit(): void {
+    this.web.sports.getAll().subscribe(d => this.sports = d);
+  }
 
-	// Creation d'actualite
-	creationActualite = {
-		titreActualite: '',
-		dateDebutActualite: '',
-		dateFinActualite: '',
-		dateMiseEnLigneActualite: '',
-		descriptionCourteActualite: '',
-		descriptionLongueActualite: '',
-		imageActualite: '',
-	};
+  sports: Sport[];
 
-	creationActualiteForm() {
-		console.log(this.creationActualite)
-	};
+  constructor(private web: WebserviceProvider) {
+  }
+
+  creationActualiteForm(a) {
+    this.web.actualites.post({
+      dateFin: new Date(Date.parse(a.dateFin)),
+      dateDebut: new Date(Date.parse(a.dateDebut)),
+      descCourte: a.descCourte,
+      descLongue: a.descLongue,
+      titre: a.titre,
+      image: a.image,
+      dateMiseEnLigne: new Date()
+    }).subscribe(id => a.sports.forEach(s => this.web.actualites.addSport(id, s).subscribe()));
+
+  };
 
 }
