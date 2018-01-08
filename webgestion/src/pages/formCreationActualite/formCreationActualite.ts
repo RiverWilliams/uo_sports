d1e66ba69e94db8f57ce175efe27ba7c108cf7b7
Louis-Maxime <louis-maxime.credeville@etu.univ-orleans.fr>
2018-01-08 22:25:01 +0100

    Confirmation d'ajout Actualite

--- webgestion/src/pages/formCreationActualite/formCreationActualite.ts
+++ webgestion/src/pages/formCreationActualite/formCreationActualite.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Sport} from "../../common/model";
 import {WebserviceProvider} from "../../common/webservice";
 import {Adaptateur} from "../../common/adaptateur";
+import {AlertController} from "ionic-angular";
 
 @Component({
   selector: 'page-formcreationactualite',
@@ -14,10 +15,13 @@ export class formCreationActualitePage implements OnInit {
 
   sports: Sport[];
 
-  constructor(private web: WebserviceProvider) {
+  valider: boolean = false;
+
+  constructor(private web: WebserviceProvider, private alertCtrl: AlertController) {
   }
 
   creationActualiteForm(a) {
+    this.valider = true;
     this.web.actualites.post({
       dateFin: Adaptateur.stringToDate(a.dateFin),
       dateDebut: Adaptateur.stringToDate(a.dateDebut),
@@ -29,6 +33,16 @@ export class formCreationActualitePage implements OnInit {
     }).subscribe(id => {
       if (Array.isArray(a.sports))
         a.sports.forEach(s => this.web.actualites.addSport(id, s).subscribe())
+      this.alertCtrl.create({title: "Confirmation", message: "Actualité crée", buttons: [{text: "OK"}]}).present();
+    }, () => {
+      this.valider = false;
+      this.alertCtrl.create({
+        title: "Erreur",
+        message: "Une erreur c'est produite veuillez réessayer",
+        buttons: [{
+          text: 'OK',
+        }]
+      }).present();
     });
 
   };
