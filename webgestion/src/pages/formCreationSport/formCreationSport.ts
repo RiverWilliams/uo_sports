ed991777557d36fc5bffd544de576b7f7f245d92
Louis-Maxime <louis-maxime.credeville@etu.univ-orleans.fr>
2018-01-09 08:56:42 +0100

    Ajout sport a la bdd avec categories

--- webgestion/src/pages/formCreationSport/formCreationSport.ts
+++ webgestion/src/pages/formCreationSport/formCreationSport.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {AlertController} from "ionic-angular";
 import {WebserviceProvider} from "../../common/webservice";
-import {Sport} from "../../common/model";
+import {CategorieSport, Sport} from "../../common/model";
 import {Utilitaire} from "../../common/utilitaire";
 
 @Component({
@@ -12,22 +12,28 @@ export class formCreationSportPage {
 
   valider: boolean = false;
 
-  constructor(private alertCtrl: AlertController, private web: WebserviceProvider) {
+  categories: CategorieSport[];
 
+  constructor(private alertCtrl: AlertController, private web: WebserviceProvider) {
+    web.categoriesSports.getAll().subscribe(d => this.categories = d);
   }
 
-  CreationSportForm(s: Sport) {
+  CreationSportForm(s) {
     this.valider = true;
-    this.web.sports.post(s).subscribe(() => this.alertCtrl.create({
-        title: "Confirmation",
-        message: "Sport crée",
-        buttons: [{
-          text: "OK", role: 'cancel', handler: () => {
-            this.valider = false;
-            return true;
-          }
-        }]
-      }).present(), () => {
+    this.web.sports.post(s).subscribe((id) => {
+        if (Array.isArray(s.categories))
+          s.categories.forEach(s => this.web.sports.addCategorie(id, s).subscribe());
+        this.alertCtrl.create({
+          title: "Confirmation",
+          message: "Sport crée",
+          buttons: [{
+            text: "OK", role: 'cancel', handler: () => {
+              this.valider = false;
+              return true;
+            }
+          }]
+        }).present();
+      }, () => {
         this.valider = false;
         Utilitaire.createAlertErreur(this.alertCtrl).present();
       }
